7e7be8c1c4b55142274ded9ee3a0a347d0c368ba
viduni94 <viduni.ushanka@gmail.com>
2019-03-07 13:19:29 +0530

    Customer reducer testing complete

---
+++ client/__tests__/reducers/customerReducer.test.js
@@ -0,0 +1,39 @@
+import customerReducer from "../../src/reducers/customerReducer";
+import { SET_ACTIVE_CUSTOMER, RESET_ACTIVE_CUSTOMER } from "../../src/actions/types";
+import expect from "expect";
+
+const initialState = {
+  customer: null,
+  activeCustomer: null
+};
+
+describe("customer reducer", () => {
+  test("should return the initial state", () => {
+    expect(customerReducer(undefined, {})).toEqual(initialState);
+  });
+
+  test("should handle SET_ACTIVE_CUSTOMER", () => {
+    expect(customerReducer(initialState, { type: SET_ACTIVE_CUSTOMER, payload: { _id: "1", fname: "test", lname: "test", mobile: "0117711771" } })).toEqual({
+      ...initialState,
+      activeCustomer: "1",
+      customer: { _id: "1", fname: "test", lname: "test", mobile: "0117711771" }
+    });
+  });
+
+  test("should handle RESET_ACTIVE_CUSTOMER", () => {
+    expect(
+      customerReducer(
+        {
+          ...initialState,
+          activeCustomer: "1",
+          customer: { _id: "1", fname: "test", lname: "test", mobile: "0117711771" }
+        },
+        { type: RESET_ACTIVE_CUSTOMER }
+      )
+    ).toEqual({
+      ...initialState,
+      activeCustomer: null,
+      customer: null
+    });
+  });
+});
